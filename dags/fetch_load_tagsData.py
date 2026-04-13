3f9c8c676652be9cda0c1a9cc4c078553178eccc
pratik-1788 <pratikchandel788@gmail.com>
2026-04-13 23:24:11 +0530

    email-update

--- dags/fetch_load_tagsData.py
+++ dags/fetch_load_tagsData.py
@@ -1,68 +1,93 @@
-from airflow.sdk import dag,task
+from airflow.sdk import dag, task
 from datetime import datetime
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.utils.email import send_email
 
-@dag
-def load_raw_full_tag():
-    try:
 
-        @task.python
-        def create_table():
-            hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
-            sql="""create or replace table netflix_database.staging.tags(userId int,movieId int,tag varchar,timestamp varchar);"""
-            hook.run(sql)
+def notify_failure(context):
+    ti = context['task_instance']
 
-        @task.python
-        def load_s3_to_snowflake():
-            try:
-                
-                hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
-                sql="""
-                        
-                        USE WAREHOUSE TRANSFORMING;
-                        USE DATABASE netflix_database;
-                        USE SCHEMA staging;
-                        copy into netflix_database.staging.tags
-                        from @netflix_database.staging.netflixstage/tags
-                        FILE_FORMAT = (
-                        TYPE = CSV 
-                        FIELD_DELIMITER = ','
-                        SKIP_HEADER = 1
-                        )
-                        ON_ERROR = 'CONTINUE';
-                    """
-                hook.run(sql)
-                count=hook.get_records("select count(*) from netflix_database.staging.tags ")
-                return count
-            except Exception as e:
-                print(e)      
-        @task.python
-        def send_consolidated_mail(count):
-            
-            if count==0:
-                subject = f"❌ Task Failed: "
-                body =  f"""
-                Task failed or no data loaded.<br>
-                Rows Loaded: {count}
+    subject = f"❌ Task Failed: {ti.task_id}"
+    body = f"""
+    DAG: {ti.dag_id} <br>
+    Task: {ti.task_id} <br>
+    Execution Date: {context.get('logical_date')} <br>
+    Log URL: {ti.log_url} <br>
+    """
+
+    send_email(
+        to="pratikchandel799@gmail.com",
+        subject=subject,
+        html_content=body
+    )
+
+
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    on_failure_callback=notify_failure
+)
+def load_raw_full_Tag():
+
+    @task(on_failure_callback=notify_failure)
+    def create_table():
+        hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+        sql = """create or replace table netflix_database.staging.tags(
+                    userId int,
+                    movieId int,
+                    tag varchar,
+                    timestamp varchar
+                );"""
+        hook.run(sql)
+        
+
+    @task(on_failure_callback=notify_failure)
+    def load_s3_to_snowflake():
+        hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+        sql = """
+                USE WAREHOUSE TRANSFORMING;
+                USE DATABASE netflix_database;
+                USE SCHEMA staging;
+
+                copy into netflix_database.staging.tags
+                from @netflix_database.staging.netflixstage/tags
+                FILE_FORMAT = (
+                    TYPE = CSV 
+                    FIELD_DELIMITER = ','
+                    SKIP_HEADER = 1
+                )
+                ON_ERROR = 'CONTINUE';
+              """
+        hook.run(sql)
+
+        count = hook.get_records(
+            "select count(*) from netflix_database.staging.tags"
+        )[0][0]
+
+        print(count)
+        return count
+
+    @task
+    def send_consolidated_mail(count):
+        subject = f"✅ Task Successfully Completed - Rows Loaded: {count}"
+        body = f"""
+                All tasks completed successfully.<br>
+                Table: Tag<br>
+                Rows 
                 """
-            else:
-                subject = f" Task successfully completed:  and loaded rows {count}"
-                body =  f"""
-                Task completed successfully.<br>
-                Rows Loaded: {count}
-                """
-            send_email(
-                    to="pratikchandel788@gmail.com",
-                    subject=subject,
-                    html_content=body
-                )    
-    except Exception as e:
-        print(e)    
 
-    create_table=create_table() 
-    load_data=load_s3_to_snowflake()
-    send_mail=send_consolidated_mail(load_data)
+        send_email(
+            to="pratikchandel99@gmail.com",
+            subject=subject,
+            html_content=body
+        )
 
-    create_table >> send_mail
-load_raw_full_tag()
+    create = create_table()
+    load_data = load_s3_to_snowflake()
+    send_mail = send_consolidated_mail(load_data)
+
+    create >> load_data >> send_mail
+
+
+load_raw_full_Tag()
--- test.py
+++ test.py
@@ -17,3 +17,99 @@ send_email(
 #   "smtp_starttls": true,
 #   "smtp_ssl": false
 # }
+
+
+
+# from airflow.sdk import dag, task
+# from datetime import datetime
+# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+# from airflow.utils.email import send_email
+
+
+# def notify_failure(context):
+#     ti = context['task_instance']
+
+#     subject = f"❌ Task Failed: {ti.task_id}"
+#     body = f"""
+#     DAG: {ti.dag_id} <br>
+#     Task: {ti.task_id} <br>
+#     Execution Date: {context.get('logical_date')} <br>
+#     Log URL: {ti.log_url} <br>
+#     """
+
+#     send_email(
+#         to="pratikchandel788@gmail.com",
+#         subject=subject,
+#         html_content=body
+#     )
+
+
+# @dag(
+#     start_date=datetime(2024, 1, 1),
+#     schedule=None,
+#     catchup=False,
+#     on_failure_callback=notify_failure
+# )
+# def load_raw():
+
+#     @task(on_failure_callback=notify_failure)
+#     def create_table():
+#         hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+#         sql = """creat or replace table netflix_database.staging.tags(
+#                     userId int,
+#                     movieId int,
+#                     tag varchar,
+#                     timestamp varchar
+#                 );"""
+#         hook.run(sql)
+#         raise Exception("Test failure")
+
+#     @task(on_failure_callback=notify_failure)
+#     def load_s3_to_snowflake():
+#         hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+#         sql = """
+#                 USE WAREHOUSE TRANSFORMING;
+#                 USE DATABASE netflix_database;
+#                 USE SCHEMA staging;
+
+#                 copy into netflix_database.staging.tags
+#                 from @netflix_database.staging.netflixstage/tags
+#                 FILE_FORMAT = (
+#                     TYPE = CSV 
+#                     FIELD_DELIMITER = ','
+#                     SKIP_HEADER = 1
+#                 )
+#                 ON_ERROR = 'CONTINUE';
+#               """
+#         hook.run(sql)
+
+#         count = hook.get_records(
+#             "select count(*) from netflix_database.staging.tags"
+#         )[0][0]
+
+#         print(count)
+#         return count
+
+#     @task
+#     def send_consolidated_mail(count):
+#         subject = f"✅ Task Successfully Completed - Rows Loaded: {count}"
+#         body = f"""
+#                 All tasks completed successfully.<br>
+#                 Table: Tag<br>
+#                 Rows 
+#                 """
+
+#         send_email(
+#             to="pratikchandel788@gmail.com",
+#             subject=subject,
+#             html_content=body
+#         )
+
+#     create = create_table()
+#     load_data = load_s3_to_snowflake()
+#     send_mail = send_consolidated_mail(load_data)
+
+#     create >> load_data >> send_mail
+
+
+# load_raw()
